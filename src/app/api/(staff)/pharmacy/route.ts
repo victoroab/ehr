5d5bd24ee4997b9378595567e07155b986484dd3
victoroab <96608980+victoroab@users.noreply.github.com>
2023-12-15 14:51:01 +0100

    pf2

--- src/app/api/(staff)/pharmacy/route.ts
+++ src/app/api/(staff)/pharmacy/route.ts
@@ -2,25 +2,21 @@ import { prisma } from '@/lib/db'
 import { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  try {
-    const query = request.nextUrl.searchParams.get('query')!
+  const query = request.nextUrl.searchParams.get('query')!
 
-    const prescription = await prisma.consultation_history.findFirst({
-      where: {
-        patient_no: query,
-      },
-      select: {
-        prescriptions: true,
-        createdAt: true,
-      },
-      take: 1,
-      orderBy: {
-        createdAt: 'desc',
-      },
-    })
+  const prescription = await prisma.consultation_history.findFirst({
+    where: {
+      patient_no: query,
+    },
+    select: {
+      prescriptions: true,
+      createdAt: true,
+    },
+    take: 1,
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
 
-    return Response.json(prescription)
-  } catch (e) {
-    console.log(e)
-  }
+  return Response.json(prescription)
 }
